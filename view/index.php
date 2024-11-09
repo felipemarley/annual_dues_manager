b48f6c6b9d134b61affb72c6ad5ecd79fe285c49
felipemarley <111880016+felipemarley@users.noreply.github.com>
2024-11-09 20:47:33 -0300

    adicionando botões na home

--- view/index.php
+++ view/index.php
@@ -1,19 +1,34 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Devs do RN - Gerenciamento de Associados</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
     <h1>Bem-vindo ao Gerenciamento de Associados - Devs do RN</h1>
     <nav>
         <ul>
-            <li><a href="associados.php">Associados</a></li>
-            <li><a href="anuidade.php">Anuidades</a></li>
-            <li><a href="pagamento.php">Pagamentos</a></li>
+            <li>
+                <form action="associados.php" method="get">
+                    <button type="submit">Associados</button>
+                </form>
+            </li>
+            <li>
+                <form action="anuidade.php" method="get">
+                    <button type="submit">Anuidades</button>
+                </form>
+            </li>
+            <li>
+                <form action="pagamento.php" method="get">
+                    <button type="submit">Pagamentos</button>
+                </form>
+            </li>
         </ul>
     </nav>
 </body>
+
 </html>
